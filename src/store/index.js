605dc63e50392d683511d57c49030f4905ac60b4
Joseph Surin <joseph.surin@gmail.com>
2019-02-22 19:51:04 +1100

    gh-pages

--- src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from 'redux'
-import logger from 'redux-logger'
-
-import reducer from './reducers'
-
-export default createStore(reducer, applyMiddleware(logger))
--- src/store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import mainReducer from './mainReducer.js'
-
-export default combineReducers({
-	mainReducer
-})
